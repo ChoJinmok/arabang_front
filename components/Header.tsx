515a00f30c7d08945e93bf4ae6d5efeb319b399c
ChoJinmok <jjmok1127@gmail.com>
2023-02-24 00:13:56 +0900

    feat: Apply Layout component

---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div>
+      <button type="button">카카오 로그인</button>
+    </div>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,15 @@ import { Global } from '@emotion/react';
 
 import type { AppProps } from 'next/app';
 
+import Layout from '@/components/Layout';
+
 import globals from '../styles/globals';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Layout>
       <Global styles={globals} />
       <Component {...pageProps} />
-    </>
+    </Layout>
   );
 }
