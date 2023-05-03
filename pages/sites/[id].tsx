360e7e01ec8ca5b9769fa2f8f3e7d0b8ff951c40
ChoJinmok <jjmok1127@gmail.com>
2023-05-03 14:32:38 +0900

    [#11] feat: 상세페이지 헤더 레이아웃 작성

---
+++ pages/sites/[id].tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+
+import BlackButton from '@/components/sites/BlackButton';
+
+export default function Site() {
+  return (
+    <>
+      <div className="flex justify-between mb-10">
+        <button type="button">
+          <Image src="/icons/arrow-left.svg" alt="left arrow icon" width={40} height={40} />
+        </button>
+
+        <div className="flex gap-2">
+          <BlackButton name="arrow-up-right" width={24} height={24} />
+          <BlackButton name="bookmark" width={24} height={24} />
+        </div>
+      </div>
+      <h1>상세페이지</h1>
+    </>
+  );
+}
